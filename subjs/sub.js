ebbdbd45be7f00cb2127b98c29a1ecb338d8e0c9
fb2087 <93234754+fb2087@users.noreply.github.com>
2021-12-24 16:42:18 +0900

    11111

--- subjs/sub.js
+++ subjs/sub.js
@@ -1,15 +1,35 @@
 $(function(){
-    $('.blue:nth-of-type(1)').click(function(){
-        $(".iblue").fadeIn();
-        $(".igold").hide();
-        $(".isilver").hide();
+    $('.black1:nth-of-type(1)').click(function(){
+        $(".black2").show();
+        $(".cgrey2").hide();
+
+
+        $(".black").fadeIn();
+        $(".cgrey").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
     });
-    $('.gold:nth-of-type(1)').click(function(){
-        $(".igold").fadeIn();
-        $(".iblue").hide();
-        $(".isilver").hide();
+    $('.cgrey1:nth-of-type(2)').click(function(){
+        $(".cgrey2").show();
+        $(".black2").hide();
+
+        
+        $(".cgrey").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
     });
-    $('.silver:nth-of-type(1)').click(function(){
+    $('.silver:nth-of-type()').click(function(){
         $(".isilver").fadeIn();
         $(".iblue").hide();
         $(".igold").hide();
