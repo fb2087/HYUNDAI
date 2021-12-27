c053e347f8afa758d09eab37e6929535cfe31530
fb2087 <93234754+fb2087@users.noreply.github.com>
2021-12-27 17:31:23 +0900

    111

--- subjs/sub.js
+++ subjs/sub.js
@@ -2,6 +2,13 @@ $(function(){
     $('.black1:nth-of-type(1)').click(function(){
         $(".black2").show();
         $(".cgrey2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
 
 
         $(".black").fadeIn();
@@ -17,6 +24,13 @@ $(function(){
     $('.cgrey1:nth-of-type(2)').click(function(){
         $(".cgrey2").show();
         $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
 
         
         $(".cgrey").fadeIn();
@@ -29,10 +43,165 @@ $(function(){
         $(".olive").hide();
         $(".dgreen").hide();
     });
-    $('.silver:nth-of-type()').click(function(){
-        $(".isilver").fadeIn();
-        $(".iblue").hide();
-        $(".igold").hide();
-    });
     
+    $('.ggrey1:nth-of-type(3)').click(function(){
+        $(".ggrey2").show();
+        $(".black2").hide();
+        $(".cgrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".ggrey").fadeIn();
+        $(".black").hide();
+        $(".cgrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.sgrey1:nth-of-type(4)').click(function(){
+        $(".sgrey2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".white2").hide();
+        $(".cgrey2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".sgrey").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".white").hide();
+        $(".cgrey").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.white1:nth-of-type(5)').click(function(){
+        $(".white2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".gold2").hide();
+        $(".cgrey2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".white").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".gold").hide();
+        $(".cgrey").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.gold1:nth-of-type(6)').click(function(){
+        $(".gold2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".cgrey2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".gold").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".cgrey").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.blue1:nth-of-type(7)').click(function(){
+        $(".blue2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".cgrey2").hide();
+        $(".olive2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".blue").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".cgrey").hide();
+        $(".olive").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.olive1:nth-of-type(8)').click(function(){
+        $(".olive2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".cgrey2").hide();
+        $(".dgreen2").hide();
+
+        
+        $(".olive").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".cgrey").hide();
+        $(".dgreen").hide();
+    });
+
+    $('.dgreen1:nth-of-type(9)').click(function(){
+        $(".dgreen2").show();
+        $(".black2").hide();
+        $(".ggrey2").hide();
+        $(".sgrey2").hide();
+        $(".white2").hide();
+        $(".gold2").hide();
+        $(".blue2").hide();
+        $(".olive2").hide();
+        $(".cgrey2").hide();
+
+        
+        $(".dgreen").fadeIn();
+        $(".black").hide();
+        $(".ggrey").hide();
+        $(".sgrey").hide();
+        $(".white").hide();
+        $(".gold").hide();
+        $(".blue").hide();
+        $(".olive").hide();
+        $(".cgrey").hide();
+    });
 });
